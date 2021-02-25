d8109ac0387bd7ce51ca577a235c551ca2db46bd
Satria <mvpsatria10@gmail.com>
2021-02-25 18:28:55 +0800

    Initial commit

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>SatriaWeb</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+</head>
+
+<body>
+    <div class="container">
+        <div class="header">
+            <div class="top-side">
+                <img src="media/LogoSat.png" style="float: left;" width="80">
+            </div>
+            <div class="top-side">
+                <h2 style="margin-left: 8%;">Selamat datang di</h2>
+                <h3 style="margin-left: 8%;">Smartband Arena</h3>
+            </div>
+        </div>
+            <div class="top-nav">
+                <a href="#" style="padding-left: 10px; color: black;">HOME</a>
+                <a href="#" style="padding-left: 40px; color: black;">SAMSUNG</a>
+                <a href="#" style="padding-left: 40px; color: black;">XIAOMI</a>
+                <a href="#" style="padding-left: 40px; color: black;">REALME</a>
+            </div>
+            <div class="row">
+                <div class="content">
+                    <div class="col-sm-9">
+                        <div class="content-box">
+                            <img class="img-set" src="media/Samsung-Galaxy-Fit-e.jpeg">
+                            <h1>Samsung Galaxy Fit</h1>
+                            <p class="content-text">
+                                &NonBreakingSpace; &NonBreakingSpace; &NonBreakingSpace; 
+                                Samsung Galaxy fit hadir dengan desain yang elegan,
+                                Galaxy Fit hadir menggunakan layar AMOLED 0,95 inci dengan resolusi sebesar 120x240 pixel.
+                                Layarnya sangat nyaman untuk dilihat, warna-warna yang ditampilkan begitu jelas.
+                                Kita tetap bisa melihat layar dengan jelas walaupun dibawah terik matahari.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-3">
+                    <div class="right-nav">
+                        <div class="right-nav-box">
+                            <h1>INFORMASI TERKINI</h3>
+                            <p><u><a class="right-nav-text" href="#">Xiaomi Mi Band 5</a></u></p>
+                            <p><u><a class="right-nav-text" href="#">Realme Band</a></p></u>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </div>
+</body>
+
+</html>
