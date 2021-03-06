e185770fd1201571bfcbf668791329caca7bb6e8
Satria <mvpsatria10@gmail.com>
2021-03-06 14:58:16 +0800

    comit pertama

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -9,7 +9,3 @@
             }
         })
     })
-    $(".custom-file-input").on("change", function() {
-        var fileName = $(this).val().split("\\").pop();
-        $(this).siblings(".custom-file-label").addClass("selected").html(fileName);
-    });
