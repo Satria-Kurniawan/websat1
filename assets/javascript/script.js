5bbca3872b9e9cfe539c578f8183a961df83b926
Satria <mvpsatria10@gmail.com>
2021-03-06 15:04:40 +0800

    commit pertama

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,11 +1,11 @@
-    $(document).ready(function() {
-        $('#checkbox').click(function() {
-            if ($(this).is(':checked')) {
-                $('#pwd1').attr('type', 'text');
-                $('#pwd2').attr('type', 'text');
-            } else {
-                $('#pwd1').attr('type', 'password');
-                $('#pwd2').attr('type', 'password');
-            }
-        })
+$(document).ready(function() {
+    $('#checkbox').click(function() {
+        if ($(this).is(':checked')) {
+            $('#pwd1').attr('type', 'text');
+            $('#pwd2').attr('type', 'text');
+        } else {
+            $('#pwd1').attr('type', 'password');
+            $('#pwd2').attr('type', 'password');
+        }
     })
+})
