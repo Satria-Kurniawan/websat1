3b4c42f9f3f54feac550627ecdc7e9eca27a4018
Satria <mvpsatria10@gmail.com>
2021-03-06 15:07:04 +0800

    commit kedua

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
     $('#checkbox').click(function() {
+
         if ($(this).is(':checked')) {
             $('#pwd1').attr('type', 'text');
             $('#pwd2').attr('type', 'text');
